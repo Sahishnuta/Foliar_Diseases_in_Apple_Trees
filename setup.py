2af7ca3edf045fb76ae816c839e38316e8d47df4
Sahishnuta <50331193+Sahishnuta@users.noreply.github.com>
2024-10-10 08:20:20 +0200

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,9 @@
+# setup.py
+from setuptools import setup, find_packages
+
+setup(
+    name='Foliar_Diseases_In_Apple_Tree',
+    version='1.0',
+    packages=find_packages(),
+    python_requires='==3.11.2',
+)
